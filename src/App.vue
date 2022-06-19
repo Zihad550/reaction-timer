8cbf51a90392c30b961efa2ae8f14408616b627c
Zihad550 <hussainmuzahid83@gmail.com>
2022-06-19 12:40:03 +0600

    feat: ui updated

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <h1>Reaction timer</h1>
   <button @click="startGame" :disabled="isPlaying">Start</button>
-  <Block :delay="delay" v-if="isPlaying" />
+  <Block :delay="delay" v-if="isPlaying" @end="endGame" />
+  <Result v-if="showResult" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block.vue";
-
+import Result from "./components/Result.vue";
 export default {
   name: "App",
   components: {
     Block,
+    Result,
   },
   data() {
     return {
       delay: null,
       isPlaying: false,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
     startGame() {
       this.delay = 2000 + Math.random() * 5000;
+      this.showResult = false;
       this.isPlaying = true;
-      console.log(this.delay, this.isPlaying);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
@@ -34,5 +43,23 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-top: 1rem;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: {
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      rank: null,
+    };
+  },
+
+  mounted() {
+    if (this.score <= 250) {
+      this.rank = "Ninja fingers";
+    } else if (this.score <= 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
